7925bbd9482de43bd3b47b9ed9398d58ba2137c8
NatiG <nati.g5684@gmail.com>
2022-07-23 18:04:30 +0300

    Fix linter errors

--- morse.rb
+++ morse.rb
@@ -1,57 +1,55 @@
 @morse_dict = {
-    "A" => ".-",
-    "B" => "-...",
-    "C" => "-.-.",
-    "D" => "-..",
-    "E" => ".",
-    "F" => "..-.",
-    "G" => "--.",
-    "H" => "....",
-    "I" => "..",
-    "J" => ".---",
-    "K" => "-.-",
-    "L" => ".-..",
-    "M" => "--",
-    "N" => "-.",
-    "O" => "---",
-    "P" => ".--.",
-    "Q" => "--.-",
-    "R" => ".-.",
-    "S" => "...",
-    "T" => "-",
-    "U" => "..-",
-    "V" => "...-",
-    "W" => ".--",
-    "X" => "-..-",
-    "Y" => "-.--",
-    "Z" => "--..",
+  'A' => '.-',
+  'B' => '-...',
+  'C' => '-.-.',
+  'D' => '-..',
+  'E' => '.',
+  'F' => '..-.',
+  'G' => '--.',
+  'H' => '....',
+  'I' => '..',
+  'J' => '.---',
+  'K' => '-.-',
+  'L' => '.-..',
+  'M' => '--',
+  'N' => '-.',
+  'O' => '---',
+  'P' => '.--.',
+  'Q' => '--.-',
+  'R' => '.-.',
+  'S' => '...',
+  'T' => '-',
+  'U' => '..-',
+  'V' => '...-',
+  'W' => '.--',
+  'X' => '-..-',
+  'Y' => '-.--',
+  'Z' => '--..'
 }
 
-# messages.each { |key, val| puts "#{key} is #{val}" }
-def decode_char (char)
-  @morse_dict.each { |key, val|
-    if char == val
-       return key
-    end
-  }
+def decode_char(char)
+  @morse_dict.each do |key, val|
+    return key if char == val
+  end
 end
 # print decode_char(".")
 
-def decode_word (string)
-word = ''
+def decode_word(string)
+  word = ''
   string.split.each do |char|
-    word = word + decode_char(char)
+    word += decode_char(char)
   end
-  return word
+  word
 end
 
 # print decode_word ".- -.. -.."
 
-def decode_message (string)
- message = []
- string.split("   ").each do |s| message.push(decode_word(s))
-end
-return message.join(' ')
+def decode_message(string)
+  message = []
+  string.split('   ').each do |s|
+    message.push(decode_word(s))
+  end
+  message.join(' ')
 end
 
-puts decode_message("      .-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
+puts decode_message('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
